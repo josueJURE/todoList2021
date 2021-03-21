f8d43db82b0d1e01081911f056e074cb4ce6cc30
josueJURE <josue.jure@gmail.com>
2021-03-21 10:43:57 +0000

    add check box in list items

--- index.js
+++ index.js
@@ -3,24 +3,37 @@ var btn = document.getElementById("btn");
 const elInput = document.getElementById("input");
 const footer = document.querySelector("footer");
 const message = document.getElementById("message");
-const elements = document.getElementsByTagName("li");
+// const elements = document.getElementsByTagName("li");
 const elCounter = document.getElementById("counter");
 
 
 
 function createListItems() {
+  var elements = document.getElementsByTagName("li");
+  console.log(elements)
   var inputValue = elInput.value;
   var newLi = document.createElement("li");
+  var input = document.createElement("input");
+  input.type = "checkbox";
   var newText = document.createTextNode(inputValue);
+  newLi.appendChild(input);
   newLi.appendChild(newText);
   elUlList.appendChild(newLi);
-  if(inputValue === "") {
-    message.innerHTML = "field can't be left empty"
-  }
-  inputValue = ""
+  var array = Array.from(elements);
 
-  elCounter.innerHTML =+ elements.length;
-}
+
+    // elements = unique
+    console.log(elements)
+
+    if(inputValue === "") {
+      message.innerHTML = "field can't be left empty"
+    }
+    inputValue = ""
+
+    elCounter.innerHTML =+ elements.length;
+  }
+
+
 
 btn.addEventListener("click", createListItems, false);
 
@@ -55,6 +68,22 @@ elUlList.addEventListener("click", function(e){
   }
 }
 })
+
+// checkboxes.forEach(function(checkbox) {
+//   checkbox.addEventListener('change', function() {
+//     enabledSettings =
+//       Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+//       .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+//       .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+//
+//     console.log(enabledSettings)
+//   })
+// });
+
+
+
+
+
 
 
 var date = new Date().toLocaleDateString("en-US")
