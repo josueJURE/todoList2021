ee5b81a53d476e44f5fe2aa371816b74ec530ee7
josueJURE <josue.jure@gmail.com>
2021-05-12 12:06:23 +0100

    modify code to enable list elements to go from to-do-list to completed-tasks-list

--- index.js
+++ index.js
@@ -3,25 +3,39 @@ var btn = document.getElementById("btn");
 const elInput = document.getElementById("input");
 const footer = document.querySelector("footer");
 const message = document.getElementById("message");
-// const elements = document.getElementsByTagName("li");
 const elCounter = document.getElementById("counter");
-
-
+const taskCompleted = document.getElementById("taskCompleted");
+var elements = document.getElementsByTagName("li");
+var input = document.getElementsByTagName("input")
 
 function createListItems() {
-  var elements = document.getElementsByTagName("li");
   var inputValue = elInput.value;
   if(inputValue === "" || inputValue === null) return
+  // var elements = document.getElementsByTagName("li");
   var newLi = document.createElement("li");
+  newLi.id = Date.now().toString();
   var input = document.createElement("input");
   input.type = "checkbox";
+  input.name = "to-do-input";
+  // var toDoInput = document.getElementsByTagName("to-do-input")
   var newText = document.createTextNode(inputValue);
   newLi.appendChild(input);
   newLi.appendChild(newText);
   elUlList.appendChild(newLi);
+  elCounter.innerHTML =+ elements.length;
+
+  var toDoInput = document.getElementsByTagName("to-do-input");
+
+  for(var i = 0; i < toDoInput.length; i++) {
+    toDoInput[i].addEventListener("change", function(e) {
+      if(e.target.checked == true) {
+        e.target.parentNode.remove();
+      }
+    })
+  }
 
 
-    elCounter.innerHTML =+ elements.length;
+
   }
 
 btn.addEventListener("click", createListItems, false);
@@ -39,23 +53,28 @@ function emptyField() {
   this.value = "";
 }
 
-elUlList.addEventListener("dblclick", function(e){
+elUlList.addEventListener("change", function(e){
   var target = e.target;
   var parent = target.parentNode;
-  alert("are you sure you want to remove this item from the list")
-  parent.removeChild(target)
+  console.log(parent)
+  console.log(target)
+
+  alert("are you sure you want to remove this item to completed task");
+  taskCompleted.appendChild(parent);
+
   elCounter.innerHTML =+ elements.length;
 });
+
+
+elUlList.addEventListener("click", strikeOutElement, false);
+
 
 function strikeOutElement(e) {
   var target = e.target;
   if(target.matches("input[type=checkbox]")){
-    target.closest("li").classList.toggle("lineThrough", target.checked)
+  target.closest("li").classList.toggle("lineThrough", target.checked);
   }
 }
-
-elUlList.addEventListener("click", strikeOutCheckedItem, false)
-
 
 var date = new Date().toLocaleDateString("en-US")
 
